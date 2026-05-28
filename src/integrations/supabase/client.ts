42b6f2d0bd332b51f33bba47f4ef9cd28e1da28e
GABRIEL FELIPE DE SOUZA RIBEIRO <GABRIELFELIPERIBEIRO080@GMAIL.COM>
2026-05-28 08:57:22 -0300

    chore: atualizando credenciais do supabase

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://kknzgvfzhmmcfuyxgzkv.supabase.co";
+const SUPABASE_URL = "https://rintyyxvllqsbikuocjb.supabase.co";
 const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrbnpndmZ6aG1tY2Z1eXhnemt2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk3ODk0OTMsImV4cCI6MjA5NTM2NTQ5M30.53CPuawCLzkwFR_oIb88wXDCV-nKMGAHvzOvElR1C2c";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpbnR5eXh2bGxxc2Jpa3VvY2piIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4NDAwNjUsImV4cCI6MjA5NTQxNjA2NX0.mYo_2qtJnSCrvRU0ubNg-y0zpVIennvQSghgKKmi56c";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
